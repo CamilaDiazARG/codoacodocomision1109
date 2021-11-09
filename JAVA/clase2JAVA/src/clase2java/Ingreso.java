2035e75d46fda1a55901d69a701d46d25c1b8713
Camila Diaz <89204918+CamilaDiazARG@users.noreply.github.com>
2021-11-08 22:49:50 -0300

    JAVA clase 2

---
+++ JAVA/clase2JAVA/src/clase2java/Ingreso.java
@@ -0,0 +1,31 @@
+
+package clase2java;
+
+
+public class Ingreso {
+    private String usuario,pass;
+    private String miUsuario="Cami";
+    private String miPass="1234";
+  
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(String us) {
+        this.usuario = us;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    public boolean Validar () {
+        return miUsuario.equals(this.usuario) && miPass.equals(this.pass);
+    }
+ 
+}
