1a4e35ce79c89c55be9a93b5db4a62b62c5f586f
camiiechelon <you@example.com>
2021-11-16 00:18:29 -0300

    act java

---
+++ JAVA/basededatos/src/main/java/config/baseDeDatos.java
@@ -0,0 +1,15 @@
+
+package config;
+
+import java.sql;
+
+
+public class baseDeDatos {
+    public String driver = "com.mysql.jdbc.Driver";
+    public Conecction getConection(){
+        Connection conecxion-null;
+        try{
+            
+        }
+    }
+}
